90f0f119bbf88322ddeddbb4194a1f98a3970bc3
meirin8770 <154606994+meirin8770@users.noreply.github.com>
2023-12-22 14:01:11 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -2,16 +2,18 @@
 # Used following archinstall. Some missing components will have to be manually installed otherwise, for a functional system.
 # Bootloader: GRUB; Audio server: Pipewire; Network: NetworkManager
 
-sudo pacman -Syu nvidia git python-pip python-pyquery hyprland nerd-fonts noto-fonts noto-fonts-cjk ttf-jetbrains-mono audacious noto-fonts-emoji ufw fail2ban nemo swaybg steam dunst viewnior gimp pavucontrol neofetch libreoffice-still waybar grub-customizer btop
+sudo pacman -Syu nvidia git python-pip python-pyquery hyprland nerd-fonts noto-fonts noto-fonts-cjk ttf-jetbrains-mono audacious noto-fonts-emoji ufw fail2ban nemo swaybg steam dunst viewnior gimp pavucontrol neofetch libreoffice-still waybar grub-customizer btop rsync
 
 mkdir git && cd git
 git clone https://aur.archlinux.org/paru.git
 cd paru/ && makepkg -si
 paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick kvantum playerctl polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 qt5-wayland qt6-wayland tumbler xdg-desktop-portal-hyprland-git xdotool cliphist qt5-imageformats qt5ct mullvad-vpn rofi-lbonn-wayland cava waypaper-git wlogout
-# Some potentially unnecessary stuff like mullvad-vpn. Most are taken from https://github.com/linuxmobile/hyprland-dots then modified to minimize bloat.
+# Some potentially unnecessary stuff like mullvad-vpn. Most are taken from https://github.com/linuxmobile/hyprland-dots with some packages removed.
 
 cd && cd git/ && git clone https://aur.archlinux.org/wezterm-git.git
 cd wezterm-git/ && makepkg -si
 
 # After all is done, place the config files in and replace the ones in $HOME/.config.
-# Can be further automated with rsync
+git clone https://github.com/meirin8770/hyprland-dots $HOME/Downloads/hyprland-dots/
+cd $HOME/Downloads/hyprland-dots/
+rsync -avxHAXP --exclude '.git*' .* ~/
