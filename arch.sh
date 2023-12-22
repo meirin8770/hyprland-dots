014ce3c27e3a571ac47c6916925acbae20c716d9
meirin8770 <154606994+meirin8770@users.noreply.github.com>
2023-12-22 14:30:43 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,13 +1,12 @@
 #! /usr/bin/bash
-# Used following archinstall. Some missing components will have to be manually installed otherwise, for a functional system.
-# Bootloader: GRUB; Audio server: Pipewire; Network: NetworkManager
 
-sudo pacman -Syu nvidia git python-pip python-pyquery hyprland nerd-fonts noto-fonts noto-fonts-cjk ttf-jetbrains-mono audacious noto-fonts-emoji ufw fail2ban nemo swaybg steam dunst viewnior gimp pavucontrol neofetch libreoffice-still waybar grub-customizer btop rsync
+sudo pacman -Syu nvidia git python-pip python-pyquery hyprland nerd-fonts noto-fonts noto-fonts-cjk ttf-jetbrains-mono audacious noto-fonts-emoji ufw fail2ban nemo swaybg steam dunst viewnior gimp pavucontrol neofetch libreoffice-still waybar grub-customizer btop rsync firefox 
 
 mkdir git && cd git
 git clone https://aur.archlinux.org/paru.git
 cd paru/ && makepkg -si
-paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick kvantum playerctl polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 qt5-wayland qt6-wayland tumbler xdg-desktop-portal-hyprland-git xdotool cliphist qt5-imageformats qt5ct mullvad-vpn rofi-lbonn-wayland cava waypaper-git wlogout
+paru -S colord ffmpegthumbnailer gnome-keyring grimblast-git gtk-engine-murrine imagemagick kvantum playerctl polkit-kde-agent qt5-quickcontrols qt5-quickcontrols2 qt5-wayland qt6-wayland tumbler xdg-desktop-portal-hyprland-git xdotool cliphist qt5-imageformats qt5ct mullvad-vpn rofi-lbonn-wayland cava waypaper-git wlogout catppuccin-gtk-theme-macchiato catppuccin-gtk-theme-mocha papirus-icon-theme sddm-git swaylock-effects-git kvantum kvantum-theme-catppuccin-git pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack wireplumber gst-plugin-pipewire
+
 # Some potentially unnecessary stuff like mullvad-vpn. Most are taken from https://github.com/linuxmobile/hyprland-dots with some packages removed.
 
 cd && cd git/ && git clone https://aur.archlinux.org/wezterm-git.git
